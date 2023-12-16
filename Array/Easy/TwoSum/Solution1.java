945bedb9be42be5cd537022681556a7bd9e87d70
rakeshveerasamy <48862315+rakeshveerasamy@users.noreply.github.com>
2023-12-16 12:59:10 +0530

    Create Solution1.java

---
+++ Array/Easy/TwoSum/Solution1.java
@@ -0,0 +1,15 @@
+//Brute force - Array is unsorted   
+//Time Complexity - O(n^2)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i =0;i<nums.length-1;i++) {
+			for(int j = i+1;j<nums.length;j++) {
+				if(nums[i] +nums[j] == target) {
+					return new int [] {i,j};
+				}
+			}
+		}
+        throw new IllegalArgumentException ("not a valid array passed");
+    }
+}
