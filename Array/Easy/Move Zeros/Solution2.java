377911dbca471feaf2f54b87380e3dc057b83309
rakeshveerasamy <48862315+rakeshveerasamy@users.noreply.github.com>
2023-12-23 09:42:06 +0530

    Update Solution2.java

--- Array/Easy/Move Zeros/Solution2.java
+++ Array/Easy/Move Zeros/Solution2.java
@@ -1,27 +1,16 @@
-public class Solution {
-	private void moveZeros(int [] arr ) {
-		int nonZeroIndex = 0;
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length == 1) return;
+        int nonZeroIndex = 0;
 		int i;
-		for( i = 0 ;i<arr.length;i++) {
-			if(arr[i]!=0) {
-			   arr[nonZeroIndex] = arr[i];
+		for( i = 0 ;i<nums.length;i++) {
+			if(nums[i]!=0) {
+			   nums[nonZeroIndex] = nums[i];
 			   nonZeroIndex++;
 			}
 		}
-		for(i = nonZeroIndex;i<arr.length;i++) {
-			arr[i] = 0;
+		for(i = nonZeroIndex;i<nums.length;i++) {
+			nums[i] = 0;
 		}
-	}
-	private void printArray(int [] arr ) {
-		for(int i : arr ) {
-			System.out.print(i+ “ “);
-		}
-	}
-public static void main (String [] args) throws Exception {
-      Solution sol = new Solution ();
-      int [] arr =  new int [] {1,0 , 0, 3,12};
-      sol.printArray(arr);
-      sol.moveZeros(arr);
-      sol.printArray(arr);
-}	
+    }
 }
