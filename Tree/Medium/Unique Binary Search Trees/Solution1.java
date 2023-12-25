abf1f80b6ab2b8ace0577a7fd3243ab0c123d976
rakeshveerasamy <48862315+rakeshveerasamy@users.noreply.github.com>
2023-12-25 13:23:01 +0530

    Create Solution1.java

---
+++ Tree/Medium/Unique Binary Search Trees/Solution1.java
@@ -0,0 +1,16 @@
+//Approach - catalan number 
+//Time Complexity - O(n)
+//Space Complexity - O(n+1)
+
+class Solution {
+    public int numTrees(int n) {
+        int [] dp = new int [n+1];
+        dp[0] = dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            for(int j = 0;j<i;j++){
+                dp[i]+=dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+}
