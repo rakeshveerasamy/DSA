735c75b6fc5408c36d0e8e4a268f809999b71d62
rakeshveerasamy <48862315+rakeshveerasamy@users.noreply.github.com>
2024-05-26 14:13:41 +0530

    Create Solution.java

---
+++ DynamicProgramming/Hard/Best Time to Buy and Sell Stock IV/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int size = prices.length;
+        int [][][] dp = new int [size+1][2][k+1];
+
+
+        for(int index=size-1;index>=0;index--){
+            for(int buy = 0;buy<=1;buy++){
+                for(int transaction =1;transaction<=k;transaction++){
+
+                    int profit =0;
+                    if(buy == 1){
+                        profit = Math.max(-prices[index]+dp[index+1][0][transaction],
+                                    0+dp[index+1][1][transaction]);
+                    }
+                    else{
+                        profit = Math.max(prices[index]+dp[index+1][1][transaction-1],
+                                    0+dp[index+1][0][transaction]);
+                    }
+                    dp[index][buy][transaction] =  profit;
+                }
+            }
+        }
+
+        return dp[0][1][k];
+    }
+}
