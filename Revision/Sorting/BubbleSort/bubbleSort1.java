727ce8898fa071343213067f33895b0b9bcdcf90
rakeshveerasamy <48862315+rakeshveerasamy@users.noreply.github.com>
2026-05-23 12:11:57 +0530

    Create bubbleSort1.java

---
+++ Revision/Sorting/BubbleSort/bubbleSort1.java
@@ -0,0 +1,50 @@
+//Time Complexity = O(n^2)
+//Space Complexity = O(1)
+
+//Stable Sort
+
+
+class BubbleSort {
+    private int[] arr;
+
+    BubbleSort(int[] arr) {
+        this.arr = arr;
+    }
+
+    private boolean isValid() {
+
+        return this.arr != null && this.arr.length > 0;
+    }
+
+    public void sort() {
+        if (!isValid()) {
+            return;
+        }
+
+        for (int i = this.arr.length - 1; i >= 1; i--) {
+            boolean swap = false;
+            for (int j = 0; j <= i-1; j++) {
+                if (this.arr[j] > this.arr[j + 1]) {
+                    swap(j, j + 1);
+                    swap = true;
+                }
+            }
+
+            if (!swap) {
+                return;
+            }
+        }
+    }
+
+    private void swap(int i, int j) {
+        int temp = this.arr[i];
+        this.arr[i] = this.arr[j];
+        this.arr[j] = temp;
+    }
+
+    public void print() {
+        for (int ele : this.arr) {
+            System.out.print(ele + " ");
+        }
+    }
+}
