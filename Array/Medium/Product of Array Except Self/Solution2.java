50ca5eb395aa879b4f2681e2aaa22ae5edea8b3b
rakeshveerasamy <48862315+rakeshveerasamy@users.noreply.github.com>
2026-07-24 09:48:36 +0530

    Create Solution2.java

---
+++ Array/Medium/Product of Array Except Self/Solution2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int size = nums.length;
+        int product= 1;
+        int [] ans = new int[size];
+
+        ans[0] = 1;
+
+        for(int i=1;i<size;i++){
+            ans[i] = ans[i-1]*nums[i-1];
+        }
+
+        for(int i=size-1;i>=0;i--){
+            ans[i]*=product;
+            product*=nums[i];
+        }
+
+        return ans;
+        
+    }
+}
